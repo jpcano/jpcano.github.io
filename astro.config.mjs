38bc209ffea871a11cf42b174105a38959881513
Jesus Cano <jcanovel@gmail.com>
2025-03-31 20:07:35 +0100

    Add Elektor contest blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import remarkToc from 'remark-toc';
 import icon from "astro-icon";
+import rehipeFigure from "rehype-figure"
 
 export default defineConfig({
   site: 'https://jesus.engineer',
   integrations: [mdx(), sitemap(), icon()],
+  markdown: {
+    remarkPlugins: [ [remarkToc, { heading: 'contents',} ] ],
+    rehypePlugins: [rehipeFigure],
+  },
 });
