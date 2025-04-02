11d9e2d1ce4d8759651ba53e3e1bb78280ba60c8
Jesus Cano <jcanovel@gmail.com>
2025-04-03 00:35:33 +0100

    Change web title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "I'm Jesus";
+export const SITE_TITLE = "Jesus Cano";
 export const SITE_DESCRIPTION = 'Jesus Cano personal website';
 export const POST_WIDTH = 990;
