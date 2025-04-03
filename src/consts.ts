53c2e7fcca9e0600a390cbcb28684679d4c547be
Jesus Cano <jcanovel@gmail.com>
2025-04-03 12:21:38 +0100

    Improve site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Jesus Cano";
-export const SITE_DESCRIPTION = 'Jesus Cano personal website';
+export const SITE_TITLE = "Jesus Cano's personal website";
+export const SITE_DESCRIPTION = "Personal website, portfolio and blog of Edge AI and Fullstack enginner Jesus Cano. Living in Cambridge, UK. Python, C++, JavaScript, Machine Learning, AI";
 export const POST_WIDTH = 990;
