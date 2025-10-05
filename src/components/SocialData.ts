737fc8d11bedfd10fe4b58d7ee2a4da40bf70aa3
Jesus Cano <jcanovel@gmail.com>
2025-10-05 17:58:09 +0100

    Update contact details

--- src/components/SocialData.ts
+++ src/components/SocialData.ts
@@ -1,25 +1,15 @@
-const socialData: Array<{ link: string; icon: string, title:string }> = [
-    {
-      title: "GitHub",
-      link: "http://github.com/jpcano",
-      icon: "fa6-brands:github",
+const socialData: Array<{ link: string; icon: string, title: string }> = [
+  {
+    title: "GitHub",
+    link: "http://github.com/jpcano",
+    icon: "fa6-brands:github",
 
-    },
-    {
-      title: "X (Twitter)",
-      link: "https://twitter.com/RockingML",
-      icon: "fa6-brands:x-twitter",
-    },
-    {
-      title: "LinkedIn",
-      link: "https://es.linkedin.com/in/jesuscano1",
-      icon: "fa6-brands:linkedin",
-    },
-    {
-      title: "GoodReads",
-      link: "https://www.goodreads.com/review/list/65914426",
-      icon: "fa6-brands:goodreads",
-    },
-  ];
+  },
+  {
+    title: "LinkedIn",
+    link: "https://es.linkedin.com/in/jesuscano1",
+    icon: "fa6-brands:linkedin",
+  },
+];
 
-  export default socialData;
+export default socialData;
